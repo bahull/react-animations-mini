46129b439cb266fc8f4fdbc4293ac72565d58edd
Bryce Hull <bahull@live.com>
2017-11-06 13:23:41 -0600

    Completed Project

--- src/Card.js
+++ src/Card.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './Card.css';
 
 
+
 export default function Card({ todo, toggle, removeTodo }) {
     return (
-        <div className='card-container'>
+        <div className='card-container' style={{height: todo.style.height, opacity: todo.style.opacity}}>
             <div 
                 onClick={ () => toggle(todo.key) }
                 className={ todo.data.completed ? 'check toggle-green' : 'check' }
